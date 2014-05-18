14124ed56f24329f4bddfd739778f4847b4ab1e6
SouthsideHuskie <r-mcguinness@kellogg.northwestern.edu>
2014-05-18 08:33:02 -0500

    Part 1 Complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,11 @@ class PicturesController < ApplicationController
     #@picture = Picture.find_by({ :id => params[:id] })
      @picture = Picture.find_by({ :id => params[:id] })
   end
+
+  def index
+
+    #@picture = Picture.find_by({ :id => params[:id] })
+     @picture_all = Picture.all
+  end
+
 end
