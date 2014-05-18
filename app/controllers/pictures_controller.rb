1dd5a88eb63bf46a74d72aab6a205718208e99a3
SouthsideHuskie <r-mcguinness@kellogg.northwestern.edu>
2014-05-18 09:37:18 -0500

    Part 3 Complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -11,4 +11,31 @@ class PicturesController < ApplicationController
      @picture_all = Picture.all
   end
 
+  def new
+
+
+  end
+
+  def create
+    @picture=Picture.new
+    @picture.caption = params[:caption]
+    @picture.source = params[:source]
+    @picture.save
+    redirect_to("http://localhost:3000/all_pictures")
+  end
+  def update
+    @picture=Picture.find_by({:id => params[:id]})
+    @picture.caption = params[:caption]
+    @picture.source = params[:source]
+    @picture.save
+    redirect_to("http://localhost:3000/picture_details/<%@picture.id%>")
+
+  end
+
+  def delete
+    @picture=Picture.find_by({:id => params[:id]})
+    @picture.destroy
+    redirect_to("http://localhost:3000/all_pictures")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rxngif::Application.routes.draw do
 
   get("/all_pictures", { :controller => "pictures", :action => "index" })
 
+  get("/new_picture_form", { :controller => "pictures", :action => "new" })
 
+  get("/create_picture", { :controller => "pictures", :action => "create" })
+
+    get("/delete_picture/:id", { :controller => "pictures", :action => "delete" })
 end
